d276211fa030770838738d16f165355afd52663f
MarKogler <S1510564009@students.fh-wels.at>
2017-02-02 15:23:59 +0100

    Add Testdriver for SharedClassDLL

---
+++ mDecisioMat/TestTreiberSharedClassDLL/TesttreiberRuelSet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTreiberSharedClassDLL
+{
+    class TesttreiberRuelSet
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
