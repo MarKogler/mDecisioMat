a884e1527d3a467cf03c278e8aa22d0006a9efa0
Martin0201 <martin.kitzwoegerer@wibs.at>
2017-02-05 09:09:34 +0100

    Create Server; Create RuleSyncInterface

--- mDecisioMat/mDecisioMat/Program.cs
+++ mDecisioMat/mDecisioMat/Program.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿// Server
+// Der Server wurde als Konsolenapplication implementiert und weist folgende Funktion auf:
+// 
+// 
+// Martin Kitzwögerer
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
 
 namespace mDecisioMatServer
 {
@@ -10,8 +16,26 @@ namespace mDecisioMatServer
     {
         static void Main(string[] args)
         {
-            //Test2
-            Console.WriteLine("Hello World");
+            /// Erstelle den Server
+            /// Die App.config enthält alle notwendigen Server Infos
+            /// 
+
+            ServiceHost cCcyServiceHost = new ServiceHost(typeof(mDecisioMat.Converter));
+
+            // Open server
+            cCcyServiceHost.Open();
+
+            // Write to console
+            Console.WriteLine();
+            Console.WriteLine("Server is waiting for request from client!");
+            Console.WriteLine();
+
+            // Keep server open for requests until the server is closed by the user.
+            Console.WriteLine("Press Enter to close the server!");
+            Console.ReadLine();
+
+            // Close server
+            cCcyServiceHost.Close();
         }
     }
 }
